3b21807ce89ee05473eaa8d32c6850e99c1599c6
Aiyzihk <mac@MACs-MacBook-Pro.local>
2024-11-13 15:38:39 +0100

    profile update 2024

--- contact-form-process.php
+++ contact-form-process.php
@@ -2,7 +2,7 @@
 if (isset($_POST['Email'])) {
 
     // EDIT THE FOLLOWING TWO LINES:
-    $email_to = "isaaconah991@gmail.com.com";
+    $email_to = "isaaconah6@gmail.com.com";
     $email_subject = "New form submissions";
 
     function problem($error)
